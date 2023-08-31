ad97594cc0d968802df557bd7873e8b77f1ec3c9
SilviaAmAm <silvia@maykinmedia.nl>
2023-08-31 12:49:24 +0200

    :bug: [#15] Set PreRequestClientContext on the client

--- prefill_haalcentraalhr/models.py
+++ prefill_haalcentraalhr/models.py
@@ -3,7 +3,8 @@ import logging
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from openforms.pre_requests.clients import PreRequestZGWClient
+from openforms.pre_requests.clients import PreRequestClientContext, PreRequestZGWClient
+from openforms.submissions.models import Submission
 from solo.models import SingletonModel
 from zgw_consumers.constants import APITypes
 
@@ -32,9 +33,13 @@ class HaalCentraalHRConfig(SingletonModel):
     class Meta:
         verbose_name = _("Haal Centraal HR configuration")
 
-    def build_client(self) -> PreRequestZGWClient | None:
+    def build_client(self, submission: Submission = None) -> PreRequestZGWClient | None:
         if not self.service:
             logger.info("No service configured for Haal Centraal HR.")
             return
 
-        return self.service.build_client()
+        client = self.service.build_client()
+        if submission:
+            client.context = PreRequestClientContext(submission=submission)
+
+        return client
